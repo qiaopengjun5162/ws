036cfb3c614ef14bbf12edd86326f6b3b87e76e2
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-06-04 00:35:23 +0800

    Rust Web 全栈开发之编写 WebAssembly 应用

--- webservice/src/bin/teacher-service.rs
+++ webservice/src/bin/teacher-service.rs
@@ -1,5 +1,5 @@
-use crate::errors::MyError;
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http, web, App, HttpServer};
 use dotenv::dotenv;
 use sqlx::postgres::PgPoolOptions;
 use std::env;
@@ -19,6 +19,7 @@ mod routers;
 #[path = "../state.rs"]
 mod state;
 
+use errors::MyError;
 use routers::*;
 use state::AppState;
 
@@ -28,14 +29,22 @@ async fn main() -> io::Result<()> {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
     let db_pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
-
     let shared_data = web::Data::new(AppState {
         health_check_response: "I'm Ok.".to_string(),
         visit_count: Mutex::new(0),
-        // courses: Mutex::new(vec![]),
         db: db_pool,
     });
     let app = move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:8080/")
+            .allowed_origin_fn(|origin, _req_head| {
+                origin.as_bytes().starts_with(b"http://localhost")
+            })
+            .allowed_methods(vec!["GET", "POST", "DELETE"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
             .app_data(shared_data.clone())
             .app_data(web::JsonConfig::default().error_handler(|_err, _req| {
@@ -43,6 +52,7 @@ async fn main() -> io::Result<()> {
             }))
             .configure(general_routes)
             .configure(course_routes) // 路由注册
+            .wrap(cors)
             .configure(teacher_routes)
     };
 
